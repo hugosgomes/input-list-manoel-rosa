70a2626675ac0358178f1a294193f55254ebf5a4
Hugo da Silva Gomes <hdsg0807@gmail.com>
2022-02-11 09:04:30 -0300

    include columns table and fixs

--- script.js
+++ script.js
@@ -19,7 +19,12 @@ inputsDropdown.forEach(function (input) {
     const inputValue = event.target.value;
     dropdownToggleItems.forEach((item) => {
       if (event.target.id === "txtPesquisaNome") {
-        if (item.querySelector("h1").textContent.indexOf(inputValue) == -1) {
+        if (
+          item
+            .querySelector("h1")
+            .textContent.toLocaleLowerCase()
+            .indexOf(inputValue.toLocaleLowerCase()) == -1
+        ) {
           item.style.display = "none";
         } else {
           item.style.display = "flex";
@@ -34,7 +39,6 @@ inputsDropdown.forEach(function (input) {
     });
   });
   input.addEventListener("focusout", function () {
-    console.log(dropdownToggle);
     dropdownToggle.classList.remove("active");
   });
 });
@@ -52,13 +56,71 @@ dropdownToggleButtons.forEach(function (dropdownToggleButton) {
       .closest(".dropdown-toggle-item")
       .querySelector("h1").textContent;
     const dropdownToggle = event.target.closest(".dropdown-toggle");
-    const cpf = event.target
+    let cpf = event.target
       .closest(".dropdown-toggle-item")
       .querySelector("h3").textContent;
     const input = event.target.closest(".input-column").querySelector("input");
     input.value = name;
     dropdownToggle.classList.remove("active");
-    table.querySelector(".cpf-value").textContent = cpf.replace("CPF ", "");
+    cpf = cpf.replace("CPF ", "");
+    table.insertAdjacentHTML(
+      "beforeend",
+      `<div class="table-row">
+    <div class="table-column table-column-40">
+      <div class="table-cell">
+        <span class="name-value">${name}</span>
+      </div>
+    </div>
+    <div class="table-column table-column-40">
+      <div class="table-cell">
+        <span class="cpf-value">${cpf}</span>
+      </div>
+    </div>
+    <div class="table-column table-column-20">
+      <div class="table-cell">
+        <span>R$</span>
+        <input type="text" class="value-benefit" name="" id="">
+      </div>
+    </div>
+    <div class="table-column table-column-20">
+      <div class="table-cell">
+        <span>R$</span>
+        <input type="text" class="value-benefit" name="" id="">
+      </div>
+    </div>
+    <div class="table-column table-column-20">
+      <div class="table-cell">
+      </div>
+    </div>
+    <div class="table-column table-column-20">
+      <div class="table-cell">
+      </div>
+    </div>
+    <div class="table-column table-column-20">
+      <div class="table-cell">
+        <img src="https://d1icd6shlvmxi6.cloudfront.net/gsc/IO90DH/44/a7/fb/44a7fbeb854f4faaa39aac086a23d3a6/images/editar_pedido_1/u54.svg?token=d1e58f9c66bcc664c821a3da1017c679f3fcd987855e953bdc1566938d49b533&pageId=fcb19f6d-571a-4ab4-a49e-85b9a5bfef78" alt="">
+        <p>Pré-pago</p>
+      </div>
+    </div>
+    <div class="table-column table-column-10">
+      <div class="table-cell">
+        <img src="https://d1icd6shlvmxi6.cloudfront.net/gsc/IO90DH/44/a7/fb/44a7fbeb854f4faaa39aac086a23d3a6/images/editar_pedido_1/u174.svg?token=8095d84dea787d79406719946a44562caad67538e243bc8eca9fc89e716ee2ca&pageId=fcb19f6d-571a-4ab4-a49e-85b9a5bfef78" alt="">
+      </div>
+    </div>
+  </div>`
+    );
     table.classList.add("active");
+    const deleteRowButtons = document.querySelectorAll(
+      ".table-row .table-column:nth-child(8) img"
+    );
+    console.log(deleteRowButtons);
+    deleteRowButtons.forEach(function (deleteRowButton) {
+      deleteRowButton.addEventListener("click", function (event) {
+        const tableRow = event.target.closest(".table-row");
+        tableRow.remove();
+        document.querySelectorAll(".table-row").length === 0 &&
+          document.querySelector(".table").classList.remove("active");
+      });
+    });
   });
 });
